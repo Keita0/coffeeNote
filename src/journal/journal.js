6c91967806e127dbef2727b43fb40fd9e8f9288e
Thomas <92072274+Keita0@users.noreply.github.com>
2023-05-16 23:19:17 +0700

    Update journal.js

--- src/journal/journal.js
+++ src/journal/journal.js
@@ -13,3 +13,28 @@ function updateImagePreview() {
     preview.src = "https://via.placeholder.com/400";
   }
 }
+
+function calculateRatio(num1, num2) {
+  if (num2 === 0) {
+    return "Infinity";
+  }
+
+  const ratio = num1 / num2;
+  return ratio.toFixed(2);
+}
+
+function calculateCoffeeWaterRatio() {
+  const coffee = parseInt(document.getElementById("input-coffee").value);
+  const water = parseInt(document.getElementById("input-water").value);
+
+  const ratio = calculateRatio(coffee, water);
+
+  const ratioTextbox = document.getElementById("input-ratio");
+  ratioTextbox.value = ratio;
+}
+
+const inputCoffee = document.getElementById("input-coffee");
+const inputWater = document.getElementById("input-water");
+
+inputCoffee.addEventListener("input", calculateCoffeeWaterRatio);
+inputWater.addEventListener("input", calculateCoffeeWaterRatio);
