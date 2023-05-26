13cc9a46140a90242c38538e0a8062df054955f6
NathanApple <nathanielcandra2003@gmail.com>
2023-05-26 17:13:11 +0700

    index dan insert

--- src/journal/insert.php
+++ src/journal/insert.php
@@ -1,10 +1,71 @@
 <?php
 // connection to database
-$conn = mysqli_connect("localhost", "root", "", "coffeenote");
+include_once "../connection.php";
 
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+$sql = "SELECT * FROM varietal";
+$varietal = mysqli_query($conn, $sql);
+
+if (isset($_POST['submit'])) {
+  // $image_url = $_POST['image'] ?? '';
+  $aroma = $_POST['aroma'] ?? '';
+  $name = $_POST['name'] ?? '';
+  $varietal = $_POST['varietal'] ?? '';
+  $flavor = $_POST['flavor'] ?? '';
+  $altitude = $_POST['altitude'] ?? '';
+  $roaster = $_POST['roaster'] ?? '';
+  $roastdate = $_POST['roastdate'] ?? '';
+  $origin = $_POST['origin'] ?? '';
+  $region = $_POST['region'] ?? '';
+  $altitude = $_POST['altitude'] ?? '';
+  $farm = $_POST['farm'] ?? '';
+  $lot = $_POST['lot'] ?? '';
+  $note = $_POST['note'] ?? '';
+  $coffee = $_POST['coffee'] ?? '';
+  $water = $_POST['water'] ?? '';
+  $cwratio = $_POST['cwratio'] ?? '';
+  $method = $_POST['method'] ?? '';
+  $extractiontime = $_POST['extractiontime'] ?? '';
+  $rating = $_POST['rating'] ?? '';
+
+  // $image = $_POST['image'];
+  //Stores the filename as it was on the client computer.
+  $imagename = $_FILES['image']['name'];
+  //Stores the filetype e.g image/jpeg
+  $imagetype = $_FILES['image']['type'];
+  //Stores any error codes from the upload.
+  $imageerror = $_FILES['image']['error'];
+  //Stores the tempname as it is given by the host when uploaded.
+  $imagetemp = $_FILES['image']['tmp_name'];
+
+  //The path you wish to upload the image to
+  $imagePath = "../images/";
+  $image_url = '';
+  if(is_uploaded_file($imagetemp)) {
+    $image_url = $imagePath.$imagename;
+      if(move_uploaded_file($imagetemp, $image_url)) {
+          // echo "Sussecfully uploaded your image.";
+      }
+      else {
+          echo "Failed to move your image.";
+      }
+  }
+  else {
+      echo "Failed to upload your image.";
+  }
+
+  $sql = "INSERT INTO journal (image_url, varietal_id, name, aroma, flavor, altitude, roaster, roastdate, origin, region, farm, lot, note, coffee, water, ratio, method, extract_time, rating)
+  VALUES ('$image_url', '$varietal ', '$name', '$aroma', '$flavor', '$altitude', '$roaster', '$roastdate', '$origin', '$region', '$farm', '$lot', '$note', '$coffee', '$water', '$cwratio', '$method', '$extractiontime', '$rating')";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    header("Location: index.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
 }
+
+mysqli_close($conn);
+
 ?>
 
 <!doctype html>
@@ -16,6 +77,7 @@ if (!$conn) {
   <title>coffeeNote</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="journal.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <script src="journal.js"></script>
 </head>
 
@@ -112,26 +174,16 @@ if (!$conn) {
             <h5>Varietals</h5>
           </div>
           <select id="input-varietal" class="form-control" name="varietal" placeholder="Varietal">
-            <option value="" disabled selected>Select varietal</option>
-            <option value="1">Coffea arabica</option>
-            <option value="2">Typica</option>
-            <option value="3">Caturra</option>
-            <option value="4">Coffea liberica</option>
-            <option value="5">Geisha</option>
-            <option value="6">Robusta coffee</option>
-            <option value="7">Maragogipe Coffee</option>
-            <option value="8">Catimor</option>
-            <option value="9">Pacas</option>
-            <option value="10">Kent</option>
-            <option value="11">Ethiopian Heirloom</option>
-            <option value="12">Jamaican Blue Mountain Coffee</option>
-            <option value="13">Tabi</option>
-            <option value="14">Coffea liberica var. dewevrei</option>
-            <option value="15">Acaia</option>
-            <option value="16">S795 coffee</option>
-            <option value="17">Yellow Icatu</option>
-            <option value="18">Obata</option>
-            <option value="19">Sl 28 and sl 34</option>
+            <!-- <option value="" disabled selected>Select varietal</option> -->
+            <?php
+                if (mysqli_num_rows($varietal) > 0) {
+                    // output data of each row
+                    while($row = mysqli_fetch_assoc($varietal)) {
+
+                      echo "<option value='".$row['varietal_id']."'>".$row['varietal_name']."</option>";
+                    }
+                  }
+            ?>
           </select>
         </div>
       </div>
@@ -200,7 +252,6 @@ if (!$conn) {
       </div>
     </div>
   </div>
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <div class="container justify-content-center">
     <div class="row">
       <div class="col-md-6 mx-auto my-3">
@@ -208,6 +259,7 @@ if (!$conn) {
           <h5>Rating</h5>
         </div>
         <div class="rating-list">
+          <input type="hidden" name="rating" id="rating">
           <i class="fa fa-star fa-2x" data-index="0"></i>
           <i class="fa fa-star fa-2x" data-index="1"></i>
           <i class="fa fa-star fa-2x" data-index="2"></i>
@@ -217,6 +269,19 @@ if (!$conn) {
       </div>
     </div>
   </div>
+
+
+  <div class="container justify-content-center">
+    <div class="row">
+      <div class="col-md-6 mx-auto my-3">
+        <button type="submit" name="submit" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+
+  </form>
+
+</body>
   <script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
   <script>
     var ratedIndex = -1;
@@ -227,7 +292,7 @@ if (!$conn) {
       $('.fa-star').on('click', function() {
         ratedIndex = parseInt($(this).data('index'));
         localStorage.setItem('ratedIndex', ratedIndex);
-        saveToTheDB();
+        $('#rating').val(ratedIndex);
       });
 
       $('.fa-star').mouseover(function() {
@@ -245,18 +310,6 @@ if (!$conn) {
     });
 
 
-    function saveToTheDB() {
-      $.ajax({
-        url: "index.php",
-        method: "POST",
-        dataType: 'json',
-        data: {
-          save: 1,
-          ratedIndex: ratedIndex
-        }
-      });
-    }
-
     function setStars(max) {
       for (var i = 0; i <= max; i++)
         $('.fa-star:eq(' + i + ')').css('color', 'gold');
@@ -266,52 +319,4 @@ if (!$conn) {
       $('.fa-star').css('color', 'grey');
     }
   </script>
-
-  <div class="container justify-content-center">
-    <div class="row">
-      <div class="col-md-6 mx-auto my-3">
-        <button type="submit" name="submit" class="btn btn-primary">Save</button>
-      </div>
-    </div>
-  </div>
-
-  </form>
-  <?php
-  if (isset($_POST['submit'])) {
-    $image_url = $_POST['image'] ?? '';
-    $aroma = $_POST['aroma'] ?? '';
-    $name = $_POST['name'] ?? '';
-    $varietal = $_POST['varietal'] ?? '';
-    $flavor = $_POST['flavor'] ?? '';
-    $altitude = $_POST['altitude'] ?? '';
-    $roaster = $_POST['roaster'] ?? '';
-    $roastdate = $_POST['roastdate'] ?? '';
-    $origin = $_POST['origin'] ?? '';
-    $region = $_POST['region'] ?? '';
-    $altitude = $_POST['altitude'] ?? '';
-    $farm = $_POST['farm'] ?? '';
-    $lot = $_POST['lot'] ?? '';
-    $note = $_POST['note'] ?? '';
-    $coffee = $_POST['coffee'] ?? '';
-    $water = $_POST['water'] ?? '';
-    $cwratio = $_POST['cwratio'] ?? '';
-    $method = $_POST['method'] ?? '';
-    $extractiontime = $_POST['extractiontime'] ?? '';
-    $rating = $_POST['rating'] ?? '';
-
-    $sql = "INSERT INTO journal (image_url, varietal_id, name, aroma, flavor, altitude, roaster, roastdate, origin, region, farm, lot, note, coffee, water, ratio, method, extract_time, rating)
-    VALUES ('$image_url', '$varietal ', '$name', '$aroma', '$flavor', '$altitude', '$roaster', '$roastdate', '$origin', '$region', '$farm', '$lot', '$note', '$coffee', '$water', '$cwratio', '$method', '$extractiontime', '$rating')";
-
-    if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-  }
-
-  mysqli_close($conn);
-
-  ?>
-</body>
-
 </html>
