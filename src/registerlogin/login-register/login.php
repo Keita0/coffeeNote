6eee4e60e445d2077b18ef8d06dfe68c0d0dd4cb
ChrisShrike <91310041+ChrisShrike@users.noreply.github.com>
2023-05-29 08:09:54 +0700

    Update login.php

--- src/registerlogin/login-register/login.php
+++ src/registerlogin/login-register/login.php
@@ -28,8 +28,6 @@ if (isset($_SESSION["user"])) {
                 if (password_verify($password, $user["password"])) {
                     session_start();
                     $_SESSION["user"] = "yes";
-                    $_SESSION["userId"] = $user["id"];
-                    $_SESSION["name"] = $user["name"];
                     header("Location: index.php");
                     die();
                 }else{
